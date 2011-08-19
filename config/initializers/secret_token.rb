820ecf0913d767b832e237b5da755882711ac59d
Ryan Ferretti <ryanferretti@gmail.com>
2011-08-19 11:11:25 -0500

    first commit... base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UproarReport::Application.config.secret_token = '8868e52b308d00ae6fccf3ae4f97087dc7eed1370196caf7efa9d0ccf26a475f7a13951b3323b771eedfeb74871b6dfb4a6afc431d1df90fcacab9e9782b6c4e'
