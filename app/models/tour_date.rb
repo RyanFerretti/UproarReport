6739eb297779900ad4768773b896234c93d6b750
Ryan Ferretti <ryanferretti@gmail.com>
2011-08-25 08:02:12 -0500

    added tour date

---
+++ app/models/tour_date.rb
@@ -0,0 +1,8 @@
+class TourDate < ActiveRecord::Base
+  attr_accessible :city, :state, :venue, :date
+
+  validates_presence_of :city
+  validates_presence_of :state
+  validates_presence_of :venue
+  validates_presence_of :date
+end
