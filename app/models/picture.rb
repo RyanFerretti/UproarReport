22144f1e1c98eac4ce70bb9480e8484c6aa1b03c
Ryan Ferretti <ryanferretti@gmail.com>
2011-08-31 12:18:56 -0500

    updating path info for picture

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -11,18 +11,18 @@ class Picture < ActiveRecord::Base
                             :watermark_path => attachment.instance.logo_path
                         }
                       }
-                    }
-                    #:storage => ENV['S3_BUCKET'] ? :s3 : :filesystem,
-                    #:s3_credentials => {
-                    #  :access_key_id => ENV['S3_KEY'],
-                    #  :secret_access_key => ENV['S3_SECRET']
-                    #},
-                    #:bucket => ENV['S3_BUCKET'],
-                    #:path => ENV['S3_BUCKET'] ? ":class/:id/:style/:filename" : ":rails_root/public/assets/images/:id/:style/:basename.:extension",
-                    #:url => "/assets/images/:id/:style/:basename.:extension"
+                    },
+                    :storage => ENV['S3_BUCKET'] ? :s3 : :filesystem,
+                    :s3_credentials => {
+                      :access_key_id => ENV['S3_KEY'],
+                      :secret_access_key => ENV['S3_SECRET']
+                    },
+                    :bucket => ENV['S3_BUCKET'],
+                    :path => ENV['S3_BUCKET'] ? ":class/:id/:style/:filename" : ":rails_root/public/assets/images/:id/:style/:basename.:extension",
+                    :url => "/assets/images/:id/:style/:basename.:extension"
 
 
-  #validates_attachment_presence :image
+  validates_attachment_presence :image
   attr_accessible :report_id, :image, :image_file_name, :image_content_type, :image_file_size, :image_updated_at
 
   def logo_path
