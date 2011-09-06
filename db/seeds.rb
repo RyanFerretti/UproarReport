8e550eeff23d2b8e3e36ed0abcf4e6f4dab525f8
Ryan Ferretti <ryanferretti@gmail.com>
2011-09-06 09:45:32 -0500

    added logging

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,6 +42,8 @@ unless Rails.env.production?
   Company.create!(:name => "Ernie Ball")
   Company.create!(:name => "Zippo")
 
+  puts "#{Company.count} companies created"
+
   Company.all.each do |company|
     company.logo = File.open(File.join("#{Rails.root}","public","logos","#{company.name}.png"))
     company.save!
