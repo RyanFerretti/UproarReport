e68d2d3f589941c078aef509d0b64ff8473605e4
Ryan Ferretti <ryanferretti@gmail.com>
2011-09-07 12:49:04 -0500

    more report fixes

--- app/mailers/published_report_mailer.rb
+++ app/mailers/published_report_mailer.rb
@@ -3,6 +3,7 @@ class PublishedReportMailer < ActionMailer::Base
 
   def company_report_published_email(report)
     @report = report
+    @company_id = report.user.company_id
     contacts = report.user.company.email_contacts.select{|c|!c.email.blank?}.map(&:email)
     contacts << report.user.company.admin.email
     mail(:to => contacts, :subject => report.tour_date.full_name, :template_name => 'report_published_email')
