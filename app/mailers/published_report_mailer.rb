e1b242a957c1564ae9e7798c6b61b0ffaadfdd4a
Ryan Ferretti <ryanferretti@gmail.com>
2011-09-09 16:39:55 -0500

    updating publishing copy

--- app/mailers/published_report_mailer.rb
+++ app/mailers/published_report_mailer.rb
@@ -6,7 +6,7 @@ class PublishedReportMailer < ActionMailer::Base
     @company_id = report.user.company_id
     contacts = report.user.company.email_contacts.select{|c|!c.email.blank?}.map(&:email)
     contacts << report.user.company.admin.email
-    mail(:to => contacts, :subject => report.tour_date.full_name, :template_name => 'report_published_email')
+    mail(:to => contacts, :subject => "Uproar #{report.tour_date.full_name}", :template_name => 'report_published_email')
   end
 
   def tour_report_published_email(report)
@@ -19,7 +19,7 @@ class PublishedReportMailer < ActionMailer::Base
 
     emails.keys.each do |k|
       @company_id = k
-      mail(:to => emails[k].select{|c|!c.blank?}, :subject => @report.tour_date.full_name, :template_name => 'report_published_email')
+      mail(:to => emails[k].select{|c|!c.blank?}, :subject => "Uproar #{@report.tour_date.full_name}", :template_name => 'report_published_email')
     end
   end
 end
