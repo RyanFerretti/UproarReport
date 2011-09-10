841824fda28365f07c3976977cfe5655edcdc5cd
Ryan Ferretti <ryanferretti@gmail.com>
2011-09-10 11:55:43 -0500

    fixing email address

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = "reports@rockstaruproar.com"
+  config.mailer_sender = "Uproar Reports <reports@rockstaruproar.com>"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
